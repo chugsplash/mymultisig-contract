bf61bfd5a792722f05434077090924309b012f44
Ryan Pate <ryantpate97@gmail.com>
2023-03-08 13:17:45 -0800

    trigger

--- chugsplash/MyMultiSigFactoryWithChugSplash.ts
+++ chugsplash/MyMultiSigFactoryWithChugSplash.ts
@@ -2,7 +2,7 @@ import { UserChugSplashConfig } from '@chugsplash/core'
 
 const config: UserChugSplashConfig = {
   options: {
-    projectName: 'My Multisig 2',
+    projectName: 'My Multisig',
   },
   contracts: {
     MyMultiSigFactoryWithChugSplash: {
