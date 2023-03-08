31c7e55db79aa7b39edf6761f6d980d738485a4b
Ryan Pate <ryantpate97@gmail.com>
2023-03-08 12:12:52 -0800

    trigger

--- chugsplash/MyMultiSigFactoryWithChugSplash.ts
+++ chugsplash/MyMultiSigFactoryWithChugSplash.ts
@@ -8,7 +8,7 @@ const config: UserChugSplashConfig = {
     MyMultiSigFactoryWithChugSplash: {
       contract: 'MyMultiSigFactoryWithChugSplash',
       variables: {
-        _multiSigCount: 1,
+        _multiSigCount: 0,
         _multiSigs: {},
         _multiSigIndex: {},
         _multiSigCreatorCount: {},
@@ -18,3 +18,5 @@ const config: UserChugSplashConfig = {
     },
   },
 }
+
+export default config
